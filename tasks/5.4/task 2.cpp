bf91165b1d093ab03a50096ebc7237d1673fd7dc
Artemy Shtefan <artemiy.shtefan@gmail.com>
2022-04-02 15:29:33 +0500

    task done

---
+++ tasks/5.4/task 2.cpp
@@ -0,0 +1,11 @@
+#include "iostream"
+int main(){
+    int a,b,c;
+    int x,y;
+    std::cout<<"Введите размер кирпича: (a, b, c)";
+    std::cin>>a>>b>>c;
+    std::cout<<"Введите размер отверстия: (x, y)";
+    std::cin>>x>>y;
+
+    if (a*b <= x*y || a*c <= x*y){std::cout<<"Пролезет";}
+}
