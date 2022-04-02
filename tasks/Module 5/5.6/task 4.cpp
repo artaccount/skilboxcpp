925cf04bfe7e4c690273853c7d0b56052da66265
Artemy Shtefan <artemiy.shtefan@gmail.com>
2022-04-02 18:03:34 +0500

    не знаю что делать дальше

---
+++ tasks/Module 5/5.6/task 4.cpp
@@ -0,0 +1,17 @@
+#include "iostream"
+int main(){
+    int day;
+    std::cout<<"Введите день: ";
+    std::cin>>day;
+
+    int weekDay = 1;
+    std::cout<<"Введите день недели с которой начинается месяц: ";
+    std::cin>>weekDay;
+
+    if (day >= 1 && day <= 5 || day >= 8 && day <= 10){std::cout<<"Выходной";}
+    if (weekDay <= 7 && weekDay > 0){
+        if (day % 7 == 0 || day % 7 == 6){std::cout<<"выходной";}
+        else std::cout<<"рабочий";
+    }
+    else std::cout<<"Некорректный ввод дня недели.";
+}
