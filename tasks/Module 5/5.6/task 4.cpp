170a4dfb11120675a3ab74e8f9e143ab06e03acf
Artemy Shtefan <artemiy.shtefan@gmail.com>
2022-04-03 20:16:44 +0500

    task done

--- tasks/Module 5/5.6/task 4.cpp
+++ tasks/Module 5/5.6/task 4.cpp
@@ -9,8 +9,9 @@ int main(){
     std::cin>>weekDay;
 
     if (day >= 1 && day <= 5 || day >= 8 && day <= 10){std::cout<<"Выходной";}
+
     if (weekDay <= 7 && weekDay > 0){
-        if (day % 7 == 0 || day % 7 == 6){std::cout<<"выходной";}
+        if (day % weekDay == 0 || day % weekDay == 6){std::cout<<"выходной";}
         else std::cout<<"рабочий";
     }
     else std::cout<<"Некорректный ввод дня недели.";
