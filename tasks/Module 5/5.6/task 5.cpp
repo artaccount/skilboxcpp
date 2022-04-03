a610b684aee0c55394c849fdc865ba2b2a4ae7a1
Artemy Shtefan <artemiy.shtefan@gmail.com>
2022-04-03 20:16:11 +0500

    task done

---
+++ tasks/Module 5/5.6/task 5.cpp
@@ -0,0 +1,18 @@
+#include "iostream"
+int main(){
+    int money;
+    std::cout<<"Введите кол-во рублей: ";
+    std::cin>>money;
+
+    std::string rub = " Рубл";
+    std::string a = "ей";
+    std::string b = "ь";
+    std::string c = "я";
+
+    int modTen = money%10;
+    int modHundred = money%100;
+
+    if(modTen <= 9 && modTen >= 5 || modHundred <= 15 && modHundred >= 5 || money%10 == 0){std::cout<<money<<rub<<a;}
+    else if (money%10 == 1){std::cout<<money<<rub<<b;}
+    else if (modTen <= 4 && modTen >= 2){std::cout<<money<<rub<<c;}
+}
