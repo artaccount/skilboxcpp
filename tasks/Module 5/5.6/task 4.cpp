2adfd50f198cc811b5e619b02c9de9f1be882d1b
art <102583622+artaccount@users.noreply.github.com>
2022-04-22 15:07:25 +0500

    task done

--- tasks/Module 5/5.6/task 4.cpp
+++ tasks/Module 5/5.6/task 4.cpp
@@ -12,7 +12,8 @@ int main(){
     if (day >= 1 && day <= 5 || day >= 8 && day <= 10){std::cout<<"Выходной";}
 
     if (weekDay <= 7 && weekDay > 0){
-        if (day % weekDay == 0 || day % weekDay == 6){std::cout<<"выходной";}
+        day = (day + weekDay) -1;
+        if (day % 7 == 0 || day % 7 == 6){std::cout<<"выходной";}
         else std::cout<<"рабочий";
     }
     else std::cout<<"Некорректный ввод дня недели.";
