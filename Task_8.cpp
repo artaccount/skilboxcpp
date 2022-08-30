111aa44932012ce990d116fb594315c241978353
art <102583622+artaccount@users.noreply.github.com>
2022-08-30 20:41:11 +0500

    Task Done

---
+++ Task_8.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+int main()
+{
+	int pYear, pMonth, pDay; // person date
+	int cYear, cMonth, cDay; // current date
+	int dYear, dMonth, dDay; // different
+
+	std::cin >> pYear >> pMonth >> pDay;
+	std::cin >> cYear >> cMonth >> cDay;
+	
+	dYear = cYear - pYear;
+	dMonth = cMonth - pMonth;
+	dDay = cDay - pDay;
+
+	if (dYear <= 18)
+	{
+		if (dMonth == 0)
+		{
+			if (dDay > 0) std::cout << "ok\n";
+			else std::cout << "not ok\n";
+		}
+		else {
+			if (dMonth < 0) std::cout << "not ok\n";
+			else std::cout << "ok\n";
+		}
+	}
+	else std::cout << "ok\n";
+	std::cout << dDay << "\n" << dMonth << "\n" << dYear;
+};
