3e704ce5f12eca51e1f135fc25a3fb9be8c7e15a
art <102583622+artaccount@users.noreply.github.com>
2022-08-30 16:22:03 +0500

    Task done

---
+++ tasks/Module 5/5.6/Task_6.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+int main()
+{
+	int a, b, c;
+	int m, n, k;
+
+	std::cout << "Enther a b c";
+	std::cin >> a >> b >> c;
+	std::cout << "Enther m n k";
+	std::cin >> m >> n >> k;
+
+	if (a > b) {
+		a, b == b, a;
+	}
+	if (a > c) {
+		a, c == c, a;
+	}
+	if (b > c) {
+		b, c == c, b;
+	}
+
+
+	if (m > n) {
+		m, n == n, m;
+	}
+	if (m > k) {
+		m, k == k, m;
+	}
+	if (n > k) {
+		n, k == k, n;
+	}
+
+	if (a == m && b == n && c == k) { std::cout << "Boxes are equal"; }
+	else if (a <= m && b <= n && c <= k) { std::cout << "The first box is smaller than the second one"; }
+	else if (a >= m && b >= n && c >= k) { std::cout << "The first box is larger than the second one"; }
+	else std::cout << "Boxes are incomparable";
+};
