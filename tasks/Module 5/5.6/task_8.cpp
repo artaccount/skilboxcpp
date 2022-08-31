341a11dec55362a3ec887a724dd49489b8d873f0
art <102583622+artaccount@users.noreply.github.com>
2022-08-31 15:06:20 +0500

    Update task_8.cpp

--- tasks/Module 5/5.6/task_8.cpp
+++ tasks/Module 5/5.6/task_8.cpp
@@ -4,27 +4,18 @@ int main()
 {
 	int pYear, pMonth, pDay; // person date
 	int cYear, cMonth, cDay; // current date
-	int dYear, dMonth, dDay; // different
 
 	std::cin >> pYear >> pMonth >> pDay;
 	std::cin >> cYear >> cMonth >> cDay;
-	
-	dYear = cYear - pYear;
-	dMonth = cMonth - pMonth;
-	dDay = cDay - pDay;
 
-	if (dYear <= 18)
-	{
-		if (dMonth == 0)
-		{
-			if (dDay > 0) std::cout << "ok\n";
-			else std::cout << "not ok\n";
-		}
-		else {
-			if (dMonth < 0) std::cout << "not ok\n";
-			else std::cout << "ok\n";
-		}
+	if (pYear - pYear < 18) {
+		std::cout << "no\n";
 	}
-	else std::cout << "ok\n";
-	std::cout << dDay << "\n" << dMonth << "\n" << dYear;
+	else if (cYear - pYear > 18) {
+		std::cout << "yes\n";
+	}
+	else if (cMonth > pMonth || (cMonth == pMonth && cDay > pDay)) {
+		std::cout << "yes\n";
+	}
+	else std::cout << "no\n";
 };
